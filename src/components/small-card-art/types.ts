17df11ee1ab1c12cf5cf705b5833b3b79641f8a7
divineDuckk <assasinsmn@gmail.com>
2024-05-19 21:40:24 +0300

    fix: fix type in small-card-art

--- src/components/small-card-art/types.ts
+++ src/components/small-card-art/types.ts
@@ -2,6 +2,6 @@ import { Art } from '@/utils/interfaces/galleryIntefaces';
 
 export interface SmallCardArtProps {
   art: Art;
-  setFavs?: (favs: Art[]) => void;
+  setFavs?: React.Dispatch<React.SetStateAction<Art[]>>;
   inFavotites?: boolean;
 }
